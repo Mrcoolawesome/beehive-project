f9d214b54340cfca08bd2553cb72590cdde8433a
Devin Schutz <schutzdevin@gmail.com>
2026-08-02 12:18:55 -0600

    got the files implemented for the beelogic application

--- Components/BeeLogic/BeeLogic.cpp
+++ Components/BeeLogic/BeeLogic.cpp
@@ -1,4 +1,45 @@
 // ======================================================================
 // \title  BeeLogic.cpp
-// \brief  This is the main application manager for the project. It is active because it will be the invoker for everything. placeholder. Use fprime-util impl
+// \author devin
+// \brief  cpp file for BeeLogic component implementation class
 // ======================================================================
+
+#include "Components/BeeLogic/BeeLogic.hpp"
+
+namespace Components {
+
+// ----------------------------------------------------------------------
+// Component construction and destruction
+// ----------------------------------------------------------------------
+
+BeeLogic ::BeeLogic(const char* const compName) : BeeLogicComponentBase(compName) {}
+
+BeeLogic ::~BeeLogic() {}
+
+// ----------------------------------------------------------------------
+// Handler implementations for typed input ports
+// ----------------------------------------------------------------------
+
+void BeeLogic ::receiveWeight_handler(FwIndexType portNum, F32 weight) {
+    // TODO
+}
+
+void BeeLogic ::run_handler(FwIndexType portNum, U32 context) {
+    // TODO
+}
+
+// ----------------------------------------------------------------------
+// Handler implementations for commands
+// ----------------------------------------------------------------------
+
+void BeeLogic ::RUN_cmdHandler(FwOpcodeType opCode, U32 cmdSeq) {
+    // TODO
+    this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
+}
+
+void BeeLogic ::STOP_RUNNING_cmdHandler(FwOpcodeType opCode, U32 cmdSeq) {
+    // TODO
+    this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
+}
+
+}  // namespace Components
--- Components/BeeLogic/BeeLogic.hpp
+++ Components/BeeLogic/BeeLogic.hpp
@@ -1,4 +1,67 @@
 // ======================================================================
 // \title  BeeLogic.hpp
-// \brief  This is the main application manager for the project. It is active because it will be the invoker for everything. placeholder. Use fprime-util impl
+// \author devin
+// \brief  hpp file for BeeLogic component implementation class
 // ======================================================================
+
+#ifndef Components_BeeLogic_HPP
+#define Components_BeeLogic_HPP
+
+#include "Components/BeeLogic/BeeLogicComponentAc.hpp"
+
+namespace Components {
+
+class BeeLogic final : public BeeLogicComponentBase {
+  public:
+    // ----------------------------------------------------------------------
+    // Component construction and destruction
+    // ----------------------------------------------------------------------
+
+    //! Construct BeeLogic object
+    BeeLogic(const char* const compName  //!< The component name
+    );
+
+    //! Destroy BeeLogic object
+    ~BeeLogic();
+
+  private:
+    // ----------------------------------------------------------------------
+    // Handler implementations for typed input ports
+    // ----------------------------------------------------------------------
+
+    //! Handler implementation for receiveWeight
+    //!
+    //! Async input port to handle the weight data once the driver fetches it
+    void receiveWeight_handler(FwIndexType portNum,  //!< The port number
+                               F32 weight) override;
+
+    //! Handler implementation for run
+    //!
+    //! main input port for the application
+    void run_handler(FwIndexType portNum,  //!< The port number
+                     U32 context           //!< The call order
+                     ) override;
+
+  private:
+    // ----------------------------------------------------------------------
+    // Handler implementations for commands
+    // ----------------------------------------------------------------------
+
+    //! Handler implementation for command RUN
+    //!
+    //! RUN: This is for turning on the application or not to pull data
+    void RUN_cmdHandler(FwOpcodeType opCode,  //!< The opcode
+                        U32 cmdSeq            //!< The command sequence number
+                        ) override;
+
+    //! Handler implementation for command STOP_RUNNING
+    //!
+    //! STOP_RUNNING: This is to stop BeeLogic from running at all
+    void STOP_RUNNING_cmdHandler(FwOpcodeType opCode,  //!< The opcode
+                                 U32 cmdSeq            //!< The command sequence number
+                                 ) override;
+};
+
+}  // namespace Components
+
+#endif
